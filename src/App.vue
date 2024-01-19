cc9d3426d182f74035f16245d4beb1b8503f68bd
csslick <digitone@naver.com>
2024-01-19 18:32:53 +0900

    section1-6 click 이벤트 좋아요 기능

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
     <h3 class="bg-yellow">{{ movie.title }}</h3>
     <p>개봉: {{ movie.year }}</p>
     <p>장르: {{ movie.category }}</p>
+    <button 
+      @:click="increseLike(i)">좋아요
+    </button> 
+    <span>{{ movie.like }}</span>
   </div>
 </template>
 
@@ -18,18 +22,26 @@ export default {
           year: 2023,
           category: "액션, 드라마",
           textRed: "color: red",
+          like: 0,
         },
         {
           title: "아쿠아맨과 로스트 킹덤",
           year: 2023,
           category: '액션, 판타지, 어드벤처',
+          like: 0,
         },
         {
           title: "3일의 휴가",
           year: 2023,
           category: '판타지, 드라마',
+          like: 0,
         },
       ]
+    }
+  },
+  methods: {
+    increseLike(i) {
+      this.data[i].like += 1;
     }
   }
 }
