e42798bf98043640d311a16bc503e7577bf8ed75
csslick <digitone@naver.com>
2024-01-26 23:27:55 +0900

    19.좋아요가 클릭이 안되는 문제-고유 id

--- src/assets/movies.js
+++ src/assets/movies.js
@@ -1,5 +1,6 @@
 const data = [
   {
+    id: 0,
     title: "노량",
     year: 2023,
     category: "액션, 드라마",
@@ -7,6 +8,7 @@ const data = [
     imgUrl: './assets/노량.jpg'
   },
   {
+    id: 1,
     title: "아쿠아맨과 로스트 킹덤",
     year: 2023,
     category: '액션, 판타지, 어드벤처',
@@ -14,6 +16,7 @@ const data = [
     imgUrl: './assets/아쿠아맨.jpg'
   },
   {
+    id: 2,
     title: "3일의 휴가",
     year: 2023,
     category: '판타지, 드라마',
--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <h1>영화정보</h1>
-    <div v-for="(movie, i) in data" :key="i" class="item">
+    <div v-for="(movie) in data" :key="movie.id" class="item">
       <figure>
         <img :src="`${movie.imgUrl}`" :alt="movie.title">
       </figure>
@@ -9,12 +9,12 @@
         <h3 class="bg-yellow">{{ movie.title }}</h3>
         <p>개봉: {{ movie.year }}</p>
         <p>장르: {{ movie.category }}</p>
-        <button @:click="$emit('increseLike', i)">좋아요
+        <button @:click="$emit('increseLike', movie.id)">좋아요
         </button>
         <span>{{ movie.like }}</span>
         <p>
           <button 
-            @click="$emit('openModal', i)">상세보기</button>
+            @click="$emit('openModal', movie.id)">상세보기</button>
         </p>
       </div>
     </div>    
